561a30540befb34e3fa83605d7a9efa3e8f0524b
Márcio <marciojosegnat@gmail.com>
2020-04-21 20:04:14 -0300

    useEffect e exibir informações
    Remover o valor padrão do state repeat e defíni-lo usando useEffect
    
    Criar um useEffect para imprimir no console "repeat updated!"
     toda vez que o valor do repeat mudar
    
    Criar um (ou mais) state para contar quantas vezes cada
    botão foi clicado
    
    Exibir a informação de cliques nos dois botões

--- src/Logo.js
+++ src/Logo.js
@@ -1,12 +1,37 @@
 /*
   Componente Logo
 */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.png';
 import './Logo.css';
  
+
 const Logo = () => {
   const [repeticao, setRepeticao] = useState(1);
+
+  //Inicializa o componente com 1, o [] diz que sera executado somente uma vez 
+  useEffect(() => {
+    setRepeticao(1);
+  }, []);
+  
+  useEffect(() => {
+    console.log('Repeticao de alteracao');
+  }, [repeticao]);
+  
+  	
+  const [numeroCliquesBotoesMais, setNumeroCliquesBotoesMais] = useState(0);
+  const [numeroCliquesBotoesMenos, setNumeroCliquesBotoesMenos] = useState(0);
+
+  //funções para pegar click , para o código não fique grande no button
+  const handleClickPlus = () => {
+    setRepeticao((repeticao) => repeticao + 1);
+    setNumeroCliquesBotoesMais((count) => count + 1);
+  }
+      
+  const handleClickMinus = () => {
+    setRepeticao((repeticao) => repeticao - 1);
+    setNumeroCliquesBotoesMenos((count) => count + 1);
+  }
 
   const array_imagens = [];
 
@@ -21,12 +46,16 @@ const Logo = () => {
         {array_imagens}
       </div>
       <div className="App-logo-buttons">
-        <button disabled={repeticao === 6} onClick={() => setRepeticao((repeticao) => repeticao + 1)}>
+        <button disabled={repeticao === 6} onClick={handleClickPlus}>
           Logo +
         </button>
-        <button disabled={repeticao === 1} onClick={() => setRepeticao((repeticao) => repeticao - 1)}>
+        <button disabled={repeticao === 1} onClick={handleClickMinus}>
           Logo -
         </button>
+      </div>
+      <div>
+        <p><b>Logo +</b> O botao foi clicado {numeroCliquesBotoesMais} vezes</p>
+        <p><b>Logo -</b> O botao foi clicado {numeroCliquesBotoesMenos} vezes</p>
       </div>
     </>
   );
